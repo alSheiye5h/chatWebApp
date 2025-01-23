1a4cf6da66cc6696b31347057308a7577331e340
alSheiye5h <anasbelkhadir13@gmail.com>
2025-01-23 19:11:30 +0100

    _

--- front-end/src/pages/inbox/msger/Msger2.jsx
+++ front-end/src/pages/inbox/msger/Msger2.jsx
@@ -2,10 +2,55 @@
 
 function Msger2() {
 
+    let msgs = [
+    {        
+        msg: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        from: "self"
+    },
+    {        
+        msg: "Ipsa perspiciatis facere sunt ex officia,",
+        from: "self"
+    },
+    {        
+        msg: "voluptatibus doloribus voluptate aspernatur",
+        from: "other"
+    },
+    {       
+        msg: "tempore omnis. Quia provident",
+        from: "self"
+    },
+    {        
+        msg: "illum magnam vero aspernatur eos. Accusantium, adipisci optio?",
+        from: "other"
+    },
+    {        
+        msg: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        from: "self"
+    },
+    {        
+        msg: "Ipsa perspiciatis facere sunt ex officia,",
+        from: "self"
+    },
+    {        
+        msg: "voluptatibus doloribus voluptate aspernatur",
+        from: "other"
+    },
+    {       
+        msg: "tempore omnis. Quia provident",
+        from: "self"
+    },
+    {        
+        msg: "illum magnam vero aspernatur eos. Accusantium, adipisci optio?",
+        from: "other"
+    }
+    ]
+
 
     return (
         <div className="Msger2">
-            
+            <img src="/public/MainAfter.jpg" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio atque, voluptates illo obcaecati rerum maiores dicta, natus accusamus adipisci rem, doloribus quaerat. Quas animi corrupti repellendus repudiandae. Asperiores, accusantium deserunt.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio atque, voluptates illo obcaecati rerum maiores dicta, natus accusamus adipisci rem, doloribus quaerat. Quas animi corrupti repellendus repudiandae. Asperiores, accusantium deserunt.</p>
         </div>
     )
 }
